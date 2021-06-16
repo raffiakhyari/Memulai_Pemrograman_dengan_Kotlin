daa6208bd4c5ffc98f434c31f4f73fe0bdd1c5b0
akhyari <muhammadraffi.akhyari@gmail.com>
2021-06-16 15:05:48 +0700

    initial commit

--- Coroutines/latihan/Latihan/src/Task.kt
+++ Coroutines/latihan/Latihan/src/Task.kt
@@ -10,13 +10,13 @@
 import kotlinx.coroutines.*
 
 // TODO 1
-fun sum(valueA: Int, valueB: Int): Int {
+suspend fun sum(valueA: Int, valueB: Int): Int {
     delay(3000)
     return valueA + valueB
 }
 
 // TODO 2
-fun multiple(valueA: Int, valueB: Int): Int {
+suspend fun multiple(valueA: Int, valueB: Int): Int {
     delay(2000)
     return valueA * valueB
 }
